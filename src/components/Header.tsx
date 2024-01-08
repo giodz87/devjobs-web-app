13b5522f8c0e61afb5ea12a4d686e91b01667ed5
gio87 <gio.dz@icloud.com>
2024-01-08 13:51:46 -0500

    fix img

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,29 @@
-import sun from "../../public/assets/desktop/icon-sun.svg";
-import moon from "../../public/assets/desktop/icon-moon.svg";
-import logo from "../../public/assets/desktop/logo.svg";
-import bg from "../../public/assets/mobile/bg-pattern-header.svg";
-import mdback from "../../public/assets/tablet/bg-pattern-header.svg";
-
-import xlback from "../../public/assets/desktop/bg-pattern-header.svg";
 import { useUserContext } from "../context";
 
 export default function Header() {
   const context = useUserContext();
   return (
     <>
-      <img className="flex md:hidden" src={bg} alt="" />
       <img
-        className=" hidden md:flex md:w-full lg:hidden "
-        src={mdback}
+        className="flex md:hidden"
+        src="./public/assets/mobile/bg-pattern-header.svg"
         alt=""
       />
-      <img className=" hidden lg:flex w-full" src={xlback} alt="" />
+      <img
+        className=" hidden md:flex md:w-full lg:hidden "
+        src="./public/assets/tablet/bg-pattern-header.svg"
+        alt=""
+      />
+      <img
+        className=" hidden lg:flex w-full"
+        src="./public/assets/desktop/bg-pattern-header.svg"
+        alt=""
+      />
       <div className=" absolute top-[-20px] flex felx-row justify-between items-center h-36 w-[375px] px-6 md:w-full md:px-10  xl:px-[165px]">
-        <img src={"../../" + logo} alt="Logo" />
+        <img src="./public/assets/desktop/logo.svg" alt="Logo" />
 
         <div className="flex felx-row justify-between items-center gap-4">
-          <img src={sun} alt="" />
+          <img src="./public/assets/desktop/icon-sun.svg" alt="" />
           <div
             onClick={() => context.setSunMoon(!context.sunMoon)}
             className="w-12 h-6 bg-[#ffffff] rounded-xl relative"
@@ -33,7 +34,7 @@ export default function Header() {
               } `}
             ></div>
           </div>
-          <img src={moon} alt="" />
+          <img src="./public/assets/desktop/icon-moon.svg" alt="" />
         </div>
       </div>
     </>
