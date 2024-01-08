a085d8af30af3670eb484d6c8f9dfb00a5a6d546
gio87 <gio.dz@icloud.com>
2024-01-07 20:32:06 -0500

    fix function

--- src/App.tsx
+++ src/App.tsx
@@ -40,10 +40,8 @@ function App() {
     const companyData = data.filter(
       (item: { position: string; location: string; contract: string }) =>
         item.position.toLowerCase().includes(search || "") &&
-        item.location.toLowerCase().includes(location || "")
-      // fullTime
-      //   ? item.contract.toLowerCase().includes("full time")
-      //   : true
+        item.location.toLowerCase().includes(location || "") &&
+        (fullTime ? item.contract.toLowerCase().includes("full time") : true)
     );
 
     setDataMap([...companyData]);
